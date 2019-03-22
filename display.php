06d6b93d482b387efca8351b7a16626da4962d2e
charaftchi <tchi.lab24@gmail.com>
2019-03-22 20:06:04 +0100

    edited display

--- display.php
+++ display.php
@@ -1,3 +1,8 @@
+<div class="container-fluid bg-dark">
+    <h1 align="center" class="text-white"> © Charaf Tchi
+    </h1>
+</div>
+
 <?php    
        require 'root/cnxDB.php'; 
        require 'temp.php';
@@ -14,23 +19,23 @@
             $video = $vid_url.$row["file_name"];
             $type = $row["type"];
             ?>
-            <div class="card">
-  <div class="card-body bg-dark text-white">
-    <p class="card-text font-weight-bold" align="center"><?php echo $row["file_name"]; ?></p>
-  </div>
-  <video width="100%"  controls>
+<div class="card">
+    <div class="card-body bg-dark text-white">
+        <p class="card-text font-weight-bold" align="center"><?php echo $row["file_name"]; ?></p>
+    </div>
+    <video width="100%" controls>
         <source src="<?php echo $video; ?>" type="video/<?php echo $type; ?>">
-</video> 
-  
-            </div>
-            </div>        
-    <?php
+    </video>
+
+</div>
+</div>
+<?php
     
         }
         echo '</div> </div>';
     ?>
 <div class="container-fluid" style="margin-top:5%;">
-<a href="index.php" class="btn btn-info btn-block">
-<i class="fa fa-home" aria-hidden="true"></i>
- Home</a>
+    <a href="index.php" class="btn btn-info btn-block">
+        <i class="fa fa-home" aria-hidden="true"></i>
+        Home</a>
 </div>
